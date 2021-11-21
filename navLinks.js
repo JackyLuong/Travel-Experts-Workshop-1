a67cf965e26e2a86cd735e45469fb4fd00c57861
JackyLuong <jackyluong726@gmail.com>
2021-11-21 00:30:40 -0700

    Setting Up template and logo

---
+++ navLinks.js
@@ -0,0 +1,7 @@
+var links = ["http://localhost:8000/main.html", "http://localhost:8000/Contact.html", "http://localhost:8000/Registration.html"];
+    
+//Instantiates links to be used
+function goToLinkInPage(index)
+{
+    location.href = links [index];
+}
