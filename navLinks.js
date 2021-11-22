2e1029838d593079d73a42da7a4a917f2868d0d2
JackyLuong <jackyluong726@gmail.com>
2021-11-22 10:03:31 -0700

    Added Drop Down Menu
    When the screen is shrunk, if the user presses the 3 lines icon it will drop down a menu with all the nav buttons. If they press again it will close the drop down menu.

--- navLinks.js
+++ navLinks.js
@@ -1,5 +1,6 @@
 var links = ["http://localhost:8000/main.html", "http://localhost:8000/Contact.html", "http://localhost:8000/Registration.html"];
 var dropDownImages = ["img/DropDownIcon.svg","img/DropDownIconHovered.svg"];
+var dropDownVisible = true;
 //Instantiates links to be used
 function goToLinkInPage(index)
 {
@@ -8,4 +9,18 @@ function goToLinkInPage(index)
 function changeDropDownIcon(index)
 {
     document.getElementById("navMiniButtonIcon").src = dropDownImages[index];
+}
+function displayDropDown()
+{
+    if(dropDownVisible == true)
+    {
+        document.getElementById("navMiniMenu").style.visibility = "visible";
+        dropDownVisible = false;
+    }
+    else
+    {
+        document.getElementById("navMiniMenu").style.visibility = "hidden";
+        dropDownVisible = true;
+    }
+    
 }
