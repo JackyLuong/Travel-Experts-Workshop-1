eacfa2a50bd211c926b709e46e03644f86101c59
JackyLuong <jackyluong726@gmail.com>
2021-11-22 00:16:11 -0700

    Changed Nav Bar
    1.Edit base.css to add a font for the webpage. changed the visuals for the nav buttons and made them shrink to an icon if the window width is less than or equal to 600px. Icon color also changes when its hovered. Moved all the logos and images into the "img" folder.

--- navLinks.js
+++ navLinks.js
@@ -1,7 +1,11 @@
 var links = ["http://localhost:8000/main.html", "http://localhost:8000/Contact.html", "http://localhost:8000/Registration.html"];
-    
+var dropDownImages = ["img/DropDownIcon.svg","img/DropDownIconHovered.svg"];
 //Instantiates links to be used
 function goToLinkInPage(index)
 {
     location.href = links [index];
+}
+function changeDropDownIcon(index)
+{
+    document.getElementById("navMiniButtonIcon").src = dropDownImages[index];
 }
