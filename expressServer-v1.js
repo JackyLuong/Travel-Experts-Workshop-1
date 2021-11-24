f5f230a620b3637e33b911fcd154d9f46bbcd6e3
JackyLuong <jackyluong726@gmail.com>
2021-11-23 18:01:29 -0700

    Add comments and centralize window resize
    Add and changed comments for the header and added whenWindowResizes.ejs to write a script on each page. This checks when the window changes size and hides the drop down nav menu if the size is too big.

--- expressServer-v1.js
+++ expressServer-v1.js
@@ -1,32 +1,42 @@
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({extended: true}));
+//Reads and serves static svg,png,jpg images to be used.
 app.use(express.static("img",{"extentions": ["svg","png","jpg"]}));
+//Reads and serves static html, and css to be used.
 app.use(express.static("views",{"extensions":["html","css"]}));
+//Reads and serves jQuery, scripts and the server to be used.
 app.use(express.static("js",{"extensions":["js"]}));
+//Displays webage in port 8000.
 app.listen(8000,() => {console.log("Server Running on port 8000.")});
+//Tells server to use ejs templates to serve the webpage.
 app.set("view engine","ejs");
 
+//Serves contact page if the url extension is "/contact."
 app.use("/Contact",(req,res)=>
 {
     res.render("Contact.ejs");
 });
 
-app.use("/Registration",(req,res)=>
+//Serves SignIn page if the url extension is "/SignIn."
+app.use("/SignIn",(req,res)=>
 {
-    res.render("Registration.ejs");
+    res.render("SignIn.ejs");
 });
+
+//Serves VacationPackages page if the url extension is "/Vacation_Packages."
 app.use("/Vacation_Packages",(req,res) =>
 {
     res.render("VacationPackages.ejs");
 });
 
+//Serves Home page if the url extension is "/Home."
  app.use("/Home",(req,res) =>
 {
     res.render("main.ejs");
 });
 
-
+//Serves 404Error page if the server can't find the page that is being request.
 app.use((req, res, next) =>
 {
     res.status(404).sendFile(__dirname + "/views/404.ejs");
